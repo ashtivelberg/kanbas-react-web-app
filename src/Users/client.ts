aa0ccec7e4dd1418af0d08d90bee339f141bb9a9
ashtivelberg <arielshtivelberg@gmail.com>
2024-04-17 01:22:37 -0400

    no credentials

--- src/Users/client.ts
+++ src/Users/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const api = axios.create({withCredentials: true});
-
+// const api = axios.create({withCredentials: true});
+// axios.defaults.withCredentials = true
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
 export interface User {
@@ -8,35 +8,35 @@ export interface User {
     firstName: string, lastName: string
 };
 export const signin = async (credentials: User) => {
-    const response = await api.post(`${USERS_API}/signin`, credentials);
+    const response = await axios.post(`${USERS_API}/signin`, credentials);
     return response.data;
 };
 export const profile = async () => {
-    const response = await api.post(`${USERS_API}/profile`);
+    const response = await axios.post(`${USERS_API}/profile`);
     return response.data;
 };
 export const updateUser = async (user: any) => {
-    const response = await api.put(`${USERS_API}/${user._id}`, user);
+    const response = await axios.put(`${USERS_API}/${user._id}`, user);
     return response.data;
 };
 export const findAllUsers = async () => {
-    const response = await api.get(`${USERS_API}`);
+    const response = await axios.get(`${USERS_API}`);
     return response.data;
 };
 export const createUser = async (user: any) => {
-    const response = await api.post(`${USERS_API}`, user);
+    const response = await axios.post(`${USERS_API}`, user);
     return response.data;
 };
 export const deleteUser = async (user: any) => {
-    const response = await api.delete(
+    const response = await axios.delete(
         `${USERS_API}/${user._id}`);
     return response.data;
 };
 export const signup = async (user: any) => {
-    const response = await api.post(`${USERS_API}/signup`, user);
+    const response = await axios.post(`${USERS_API}/signup`, user);
     return response.data;
 };
 export const signout = async () => {
-    const response = await api.post(`${USERS_API}/signout`);
+    const response = await axios.post(`${USERS_API}/signout`);
     return response.data;
 };
